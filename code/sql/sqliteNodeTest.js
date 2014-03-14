c1e3a8f97672370d6ca3e4b6081581725b73b120
David Lannoye <david@lannoye.net>
2014-03-13 21:34:19 -0500

    Update sqliteNodeTest.js
    Removing old test code from SQL Example

--- code/sql/sqliteNodeTest.js
+++ code/sql/sqliteNodeTest.js
@@ -11,16 +11,6 @@ var db = new sqlite3.Database(":memory:");
   }
   stmt.finalize();
 
-  db.all("SELECT * FROM DispactLogs LIMIT 10", function(err, rows) {
-     
-     if (rows)
-     { 
-      rows.forEach(function (row) {
-          console.log(row.id + ": " + row.info + ' : '+ row.time);
-      });
-    }
-  });
-
   db.all("SELECT rowid AS id, info, time FROM lorem WHERE info LIKE ? AND time BETWEEN ? AND ?", '%Ipsum%', 3, 8
     , function(err, rows) {
      console.log('Trying param query');
